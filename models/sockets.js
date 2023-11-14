8e2ffafa8163f04c8423aeb2070cd867433636ad
Miguel <69365799+Miguel-Candia@users.noreply.github.com>
2023-11-13 23:39:08 -0300

    chat funcionando

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,5 @@
+const { usuarioConnect, usuarioDisconnect, getUsuarios, grabarMensaje } = require("../controllers/sockets");
+const { comprobarJWT } = require("../helpers/jwt");
 
 
 class Sockets {
@@ -11,9 +13,20 @@ class Sockets {
 
     socketEvents() {
         // On connection
-        this.io.on('connection', ( socket ) => {
+        this.io.on('connection', async( socket ) => {
 
-            console.log('Cliente Conectado');
+        const [valido, uid] = comprobarJWT(socket.handshake.query['x-token'])
+
+        if( !valido ){
+            console.log('Socket no identificado')
+            return socket.disconnect();
+        }
+
+        await usuarioConnect( uid );
+
+        // unir al usuario a una sala de socket.io
+        socket.join(uid);
+
 
         // Validar JWT
 
@@ -23,13 +36,29 @@ class Sockets {
 
         // Emitir todos los usuarios conectados
 
+        this.io.emit('lista-usuario', await getUsuarios())
+
+
         // Socket join (Unir a salas)
 
         // Escuchar cuando cliente manda un mensaje
+        socket.on ( 'mensaje-personal', async ( payload ) =>{
+
+            const mensaje = await grabarMensaje(payload);
+
+            this.io.to( payload.para ).emit( 'mensaje-personal', mensaje )
+            this.io.to( payload.de ).emit( 'mensaje-personal', mensaje )
+
+        } );
 
         // disconnect
         // marcar en bd que el usuario se desconecto
         // Emitir todos los usuarios conectados
+        socket.on('disconnect', async() =>{
+            console.log('Cliente Desconectado',uid);
+            await usuarioDisconnect( uid );
+            this.io.emit('lista-usuario', await getUsuarios())
+        })
             
         
         });
